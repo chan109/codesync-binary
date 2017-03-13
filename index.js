bae4ad49be2ea58882c36db65cfe659d0d8ab855
Joseph Thomas <joey6978@gmail.com>
2017-03-12 17:27:12 -0700

    Redoing from scratch

--- index.js
+++ index.js
@@ -23,25 +23,6 @@ function parse (data, cb) {
 
   // If the JSON is good, handle it properly.
   switch (json.event) {
-    case 'message':
-
-      if (json.to == 'all') {
-
-
-      }
-
-      var user
-      // Escape if the user doesn't exist.
-      if(!(user = Users[json.to])) {
-        break
-      }
-
-      // Send the data to the user.
-      user.datachannel.send(json.data)
-    break
-    case 'conn': 
-
-    break
     case 'list':
       list.forEach((user_id) => {
         Users[user_id] = {
