9bc3902e2c82d9fbd02da5d0b752be4e47b70919
Michael Chen <749827352@qq.com>
2017-03-14 23:47:06 -0700

    add comment

--- index.js
+++ index.js
@@ -20,16 +20,19 @@ function parse(data, cb) {
     switch (json.event) {
         case 'list' :
             json.payload.forEach((user_id) => {
+                //making sure connection for user_id does not exist in peerList{}
                 if(peerList[user_id] == null) {
+                    //create offer for each users_id
                     var peer = new Peer(user_id, true)
+                    //store the p2p connection for user
                     peerList[user_id] = peer
-                    console.log(peerList[user_id].active)
                 }
             })
             break
         
         case 'answer' :
             if(peerList[json.payload.from]) {
+                //signal the answer as the last step for the p2p connection
                 peerList[json.payload.from].signal(json.payload.data)
             } else {
                 console.log('The answer or the offer is unknown')
@@ -37,12 +40,16 @@ function parse(data, cb) {
             break
         
         case 'offer' :
+            //create a peer connection object with initator set to false
             var peer = new Peer(json.payload.from, false)
+            //save the peer by key
             peerList[json.payload.from] = peer
+            //signal the offer to create the answer
             peer.signal(json.payload.data)
             break
 
         case 'broadcast':
+            //send message to users who are already connected
             for(var peer in peerList) {
                 if(peerList[peer].active) {
                     peerList[peer].send(json.payload)
@@ -51,6 +58,7 @@ function parse(data, cb) {
             break
         
         case 'message':
+            //send message to a particular user
             if(peerList[json.payload.to]) {
                 peerList[json.payload.to].send(json.payload.data)
             }
--- peer.js
+++ peer.js
@@ -1,6 +1,6 @@
 function Peer (user_id, isInit) {
     const self = this
-    this.active = false
+    this.active = false     //when a p2p connection is done, active = true
     this.user_id = user_id
     const wrtc = require('wrtc')
     const Peer = require('simple-peer')
@@ -10,46 +10,35 @@ function Peer (user_id, isInit) {
         peer = new Peer({ initiator: false, wrtc: wrtc, trickle: false})
     }
 
+    //data here is the actual offer or the answer
     peer.on('signal', function (data) {
         var sdp = {"event":"conn", "payload":{"to": user_id, "data": data}}
         console.log(JSON.stringify(sdp));
     })
-    
+
+    //trigger by peer.send
+    //send data to the remote peer in the custom format
     peer.on('data', function (data) {
         console.log("(",self.user_id,")"," DATA : ", data.toString('utf8'));
     })
 
+    //p2p connection is established
     peer.on('connect', function () {
         console.log('success')
         self.active = true
-        console.log(self.active);
         peer.send("Ping");
-    })
-
-    peer.on('disconnect', function () {
-        console.log("P2P disconnet")
     })
 }
 
+//create answer or offer, it trigger peer.on('signal,......)
 Peer.prototype.signal = function(sdp) {
     peer.signal(sdp);
 }
 
+//send message to the remote peer
 Peer.prototype.send = function (data) {
     peer.send(data)
 }
-
-Peer.prototype.on = function (event, cb) {
-    var evArr = this.Events[event]
-    if (evArr) {
-        evArr.push(cb)
-    } else {
-        this.Events[event] = [cb]
-    }
-}
-
-
-
 
 module.exports = Peer
 
